e0a824dd0e680b51ae59621da937439298e41e46
Said Mg <saidmghabghab@gmail.com>
2021-02-02 00:47:12 -0500

    completed the readme file and divided the html file into html/css/js files

--- README.md
+++ README.md
@@ -2,11 +2,20 @@
 Weather forecast
 
 
-## Your Task
+## About
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+A weather dashboard that shows the weather for the current day and 5 next days by retrieving data from a weather forecast API that will run in the browser and feature dynamically updated HTML and CSS.
 
-Use the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities. The documentation includes a section called "How to start" that provides basic setup and usage instructions. You will use `localStorage` to store any persistent data.
+Used the [OpenWeather API](https://openweathermap.org/api) to retrieve weather data for cities.And included the use of `localStorage` to store  persistent data for search history.
+
+# Sample Pictures
+* Screenshots are available inside ./assets/ for more details
+![GitHub Logo](/Assets/Capture.JPG)
+
+
+# URLS
+* The URLs of the deployed applications:  https://saidmg.github.io/homework6/
+* The URLs of the GitHub repositories  :  https://github.com/saidmg/homework6.git
 
 ## User Story
 
@@ -32,61 +41,4 @@ WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
 ```
 
-## Mock-Up
 
-The following image shows the web application's appearance and functionality:
-
-![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
-
-## Grading Requirements
-
-This homework is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-    * Uses the OpenWeather API to retrieve weather data.
-
-    * Uses `localStorage` to store persistent data.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the homework instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a readme describing the project.
-
-- - -
-© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
---
+++ javascript.js
@@ -0,0 +1,94 @@
+var apiurl1
+var apiurl2
+var weatherWeekly
+var city
+var longitude
+var latitude
+var longandlat
+var uvi
+var windspeed
+var humidity
+var temperature
+var saveDays = []
+async function getWeather() {
+    if(!city){
+    city = document.querySelector('.me-2').value
+    }
+
+    apiurl1 = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&appid=166a433c57516f51dfab1f7edaed8413&units=metric`
+
+    // get the movie information
+    console.log(`... getting weather info for toronto`)
+    weatherWeekly = await fetch(apiurl1).then(r => r.json())
+    console.log(` .. fetched weather info: `, weatherWeekly)
+    if (weatherWeekly.cod != 404) {
+        document.getElementById('toggleatstart').classList.remove('hidden')
+        longitude = weatherWeekly.city.coord.lon;
+        latitude = weatherWeekly.city.coord.lat;
+        apiurl2 = `https://api.openweathermap.org/data/2.5/uvi?lat=${latitude}&lon=${longitude}&appid=166a433c57516f51dfab1f7edaed8413&units=metric`
+        longandlat = await fetch(apiurl2).then(r => r.json())
+        uvi = longandlat.value
+        windspeed = weatherWeekly.list[0].speed
+        // document.getElementById('cityNameday1').innerHTML = ``
+        document.getElementById('cityNameday1').innerHTML = `${weatherWeekly.city.name}`
+        document.getElementById('windSpeedday1').innerHTML = `Wind Speed: ${windspeed}`
+        document.getElementById('uviday1').innerHTML = `UV Index: ${uvi}`
+
+        for (i = 1; i < 7; i++) {
+            humidity = weatherWeekly.list[i - 1].humidity
+            temperature = (weatherWeekly.list[i - 1].temp.min + weatherWeekly.list[i - 1].temp.max) / 2
+            temperature = temperature.toFixed(2)
+            document.getElementById(`temperatureday${i}`).innerHTML = `Temp: ${temperature}°C`
+            document.getElementById(`humidityday${i}`).innerHTML = `Humidity: ${humidity}%`
+            document.getElementById(`day${i}icon`).src = `http://openweathermap.org/img/wn/${weatherWeekly.list[i - 1].weather[0].icon}@2x.png`
+        }
+        // for (var t = 1; t < document.querySelector('ul').children.length; t++) {
+        //     if (city === document.querySelector('ul').children[t].textContent) {
+        //         console.log('lol')
+        //     }
+        //     else { console.log('llllol') }
+        // }
+        for (var d = 0; d < JSON.parse(localStorage.getItem('saveDays')).length; d++) {
+            if (saveDays.includes(`${city}`)) {
+                console.log('Already There')
+            }
+            else {
+                const cityName = document.createElement('button')
+                cityName.setAttribute('class', ` btn btn-light ${weatherWeekly.city.name}`)
+                cityName.setAttribute('id', `${weatherWeekly.city.name}`)
+                cityName.textContent = `${weatherWeekly.city.name}`
+                document.querySelector('.list-group').appendChild(cityName)
+                document.getElementById(`${weatherWeekly.city.name}`).onclick = function () { testing(this.id) }
+
+                saveDays.push(`${weatherWeekly.city.name}`)
+                localStorage.setItem('saveDays', JSON.stringify(saveDays));
+            }
+        }
+
+
+    }
+}
+
+// }
+// }
+
+if (localStorage.getItem('saveDays')) {
+    var retrievedObject = localStorage.getItem('saveDays');
+    saveDays = JSON.parse(retrievedObject)
+    for (var i = 0; i < saveDays.length; i++) {
+        const cityName = document.createElement('button')
+        cityName.setAttribute('class', ` btn btn-light ${saveDays[i]}`)
+        cityName.setAttribute('id', `${saveDays[i]}`)
+        cityName.textContent = `${saveDays[i]}`
+        document.querySelector('.list-group').appendChild(cityName)
+        console.log(`${saveDays[i]}`)
+        document.getElementById(`${saveDays[i]}`).onclick = function () { testing(this.id) }
+
+    }
+}
+
+function testing(clicked_id) {
+    city = clicked_id
+    getWeather()
+
+}
